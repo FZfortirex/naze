8f778025c7f33e6609cab15f0fd44ba971374354
FZfortirex <99706613+FZfortirex@users.noreply.github.com>
2022-10-03 17:00:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker By'
-global.author = 'Naze'
+global.owner = ['6285728581059']
+global.ownernomer = "6285728581059"
+global.premium = ['6285728581059']
+global.packname = 'Cemara'
+global.author = 'BotWhatsApp'
 global.sessionName = 'nazedev'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
